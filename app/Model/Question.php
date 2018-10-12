c191eb0ad31be3962486cd80962b9718419a68e8
midiraj <shekh.deep@gmail.com>
2018-10-12 14:22:54 +0600

    CRUD for Category

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -9,13 +9,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+
+	protected $guarded=[];
+
 	public function getRouteKeyName()
 	{
 		return 'slug';
 	}
-
-	protected $guarded=[];
-
+	
 	public function user()
 	{
 		return $this->belongsTo(User::class);
